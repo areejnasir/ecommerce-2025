209f2e9bdae9863d7b64e141591b28d2797c5418
areejnasir <areejnasir01.com>
2025-05-15 17:38:11 +0500

    ecommerce second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'themewagon.github.io',
+        pathname: '/**', // Match all paths under the domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
